754810a464be65ab27fc5c3ba1a7951f71bdcbc8
Uvaish <uvaish.nasir@handajitech.com>
2024-09-28 12:44:39 +0530

    add react frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+function App() {
+  return (
+    <>
+      <Navbar />
+      <div className="App"></div>
+    </>
+  );
+}
+
+export default App;
